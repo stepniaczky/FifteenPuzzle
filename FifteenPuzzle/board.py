3a097db3830acbe6dc00c80aa27180b0e8896256
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-05-17 19:18:26 +0200

    adding empty cell index getter && move method to board class

--- FifteenPuzzle/board.py
+++ FifteenPuzzle/board.py
@@ -19,6 +19,7 @@ class Board:
 
     # checks if board is solved correctly, which means that board elements
     # starts with 1 then iterates one by one to 15 and the last element of board equals 0
+    # returns True if board is solved, else False
     def is_solved(self) -> bool:
         try:
             for i, element in enumerate(self.board, start=1):
@@ -29,3 +30,29 @@ class Board:
         except AssertionError:
             return False
 
+    # returns tuple of row and column index of empty cell in board
+    def get_empty_cell(self) -> tuple:
+        for i, row in enumerate(self.board):
+            if 0 in row:
+                return i, row.index(0)
+
+    # checks if move in given direction is possible, if so it returns copy of board with appropriate shift
+    # if it is not possible, method returns False
+    def move(self, direction: str) -> bool or list:
+        b = self.board.copy()
+        row, col = self.get_empty_cell()
+        try:
+            match direction:
+                case 'U':
+                    b[row][col], b[row - 1][col] = b[row - 1][col], b[row][col]
+                case 'D':
+                    b[row][col], b[row + 1][col] = b[row + 1][col], b[row][col]
+                case 'R':
+                    b[row][col], b[row][col - 1] = b[row][col - 1], b[row][col]
+                case 'L':
+                    b[row][col], b[row][col + 1] = b[row][col + 1], b[row][col]
+                case _:
+                    return False
+            return b  # if IndexError exception has not occurred, return copy of board with given shift
+        except IndexError:
+            return False
