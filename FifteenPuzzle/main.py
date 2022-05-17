d61d1be40ff1a03d99e6da4e1deba09262cf85ef
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-05-17 11:04:02 +0200

    Initial main

---
+++ FifteenPuzzle/main.py
@@ -0,0 +1,43 @@
+import sys
+from check_args import check_args
+
+
+def save(filename: str, _dict: dict):
+    with open(filename, 'w') as file:
+        for key in _dict.keys():
+            file.write(f"{key}: {_dict[key]}\n")
+
+
+def load(filename: str):
+    try:
+        with open(f"data/{filename}") as file:
+            return file.readlines()
+    except FileNotFoundError:
+        exit("File with initial board does not exist!")
+
+
+def main():
+    # args = [strategy, parameter, initial board file, results file, file with additional info]
+    args = sys.argv[1:]
+
+    if len(args) != 5:
+        exit("This script requires 5 positional arguments to start!")
+    elif check_args(args) is False:
+        exit()
+    else:
+        pass
+        # board = Board(load(args[2]))
+        # match args[0]:
+        #     case 'dfs':
+        #         strategy = DFS()
+        #     case 'bfs':
+        #         strategy = BFS()
+        #     case 'astr':
+        #         strategy = ASTR()
+        # model = Strategy(board, args[0], args[1])
+        # save(args[3], model.get_results)
+        # save(args[4], model.get_info)
+
+
+if __name__ == '__main__':
+    main()
