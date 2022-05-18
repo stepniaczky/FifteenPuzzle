1d1f819950ccb2acc424d9f180f9fd4be960565b
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-05-18 12:38:31 +0200

    adding info/results getter method to base strategy class

--- FifteenPuzzle/strategy.py
+++ FifteenPuzzle/strategy.py
@@ -2,6 +2,22 @@ from abc import ABC, abstractmethod
 
 
 class Strategy(ABC):
+    MAX_RECURSION = 20
+
+    def __init__(self):
+        self.path_length = 0
+        self.path = ''
+        self.visited = 0
+        self.processed = 0
+        self.max_recursion_reached = 0
+        self.elapsed_time = 0
+
+    def get_result(self) -> list:
+        return [self.path_length, self.path]
+
+    def get_info(self) -> list:
+        return [self.path_length, self.visited, self.processed,
+                self.max_recursion_reached, round(self.elapsed_time, 3)]
 
     @abstractmethod
     def solve(self):
