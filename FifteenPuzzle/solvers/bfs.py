356e684d422ee45915b98dd78443aea90ff8b92f
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-05-22 01:08:55 +0200

    bfs class update

--- FifteenPuzzle/solvers/bfs.py
+++ FifteenPuzzle/solvers/bfs.py
@@ -10,38 +10,40 @@ class BFS(Strategy):
         timer = time_ns()  # starting timer
         queue = [board]
         recursion_lvl = {}
-        visited = [str(board.board)]
+        visited = [hash(board)]
         is_solved = False
-        end_node: Board = board
-        self.set_rec_dict(recursion_lvl, hash(board))
+        node: Board = board
+        # self.set_rec_dict(recursion_lvl, hash(board))
+        recursion_lvl[hash(board)] = 0
 
         while queue:
             node = queue.pop(0)
             self.processed += 1
 
-            position = hash(node)
-            if recursion_lvl[position] > self.MAX_RECURSION:
+            lvl = recursion_lvl[hash(node)]
+            self.max_recursion_reached = max(lvl, self.max_recursion_reached)
+            if lvl > self.MAX_RECURSION:
                 break
 
             if node.is_solved():
-                end_node = node
                 is_solved = True
                 break
 
             for neighbour in self.get_neighbourhood(node):
-                if str(neighbour.board) not in visited:
+                if hash(neighbour) not in visited:
                     queue.append(neighbour)
-                    visited.append(neighbour)
-                    self.set_rec_dict(recursion_lvl, hash(neighbour))
+                    visited.append(hash(neighbour))
+                    # self.set_rec_dict(recursion_lvl, hash(neighbour))
+                    recursion_lvl[hash(neighbour)] = lvl + 1
 
         # when solving process is done, saves solving time
         self.elapsed_time = (time_ns() - timer) / (10 ** 6)  # nanoseconds to milliseconds
         self.visited = len(visited)
         if is_solved is True:
             s = ''
-            while end_node.parent is not None:
-                s = end_node.movement + s
-                end_node = end_node.parent
+            while node.parent is not None:
+                s = node.movement + s
+                node = node.parent
             self.path = s
             self.path_length = len(self.path)
         else:
