5db4a2d16848c6d13291d2219a4ca020d9a9137f
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-05-22 18:24:22 +0200

    dfs update

--- FifteenPuzzle/solvers/dfs.py
+++ FifteenPuzzle/solvers/dfs.py
@@ -6,48 +6,44 @@ from model.board import Board
 # Depth First Search
 class DFS(Strategy):
 
-    def __init__(self, parameter):
+    def __init__(self, parameter: str):
         super().__init__(parameter)
-        self.visited_list = []
+        self.expanded = {}
         self.break_flag = False
+        self._dict = {}
 
-    def solve(self, board):
-        timer = time_ns()  # starting timer
-        self.visited_list.append(hash(board))
-        end_node: Board = self.__dfs(board, 1)
-
-        # when solving process is done, saves solving time
+    def solve(self, board: Board):
+        timer = time_ns()
+        end_node = self.__dfs(board, 1)
         self.elapsed_time = (time_ns() - timer) / (10 ** 6)  # nanoseconds to milliseconds
-        self.visited = len(self.visited_list)
-        if self.break_flag is False:
+
+        if end_node is None:
+            self.path_length = -1
+        else:
             s = ''
             while end_node.parent is not None:
                 s = end_node.movement + s
                 end_node = end_node.parent
             self.path = s
             self.path_length = len(self.path)
-        else:
-            self.path_length = -1
 
-    def __dfs(self, node: Board, rec_lvl: int):
-        if self.break_flag is True:
-            return None
+    def __dfs(self, node: Board, recursion_lvl: int) -> Board:
 
-        if node in self.visited_list:
+        self.max_recursion_reached = max(self.max_recursion_reached, recursion_lvl)
+        if recursion_lvl > self.MAX_RECURSION:
             return None
-        else:
-            self.visited_list.append(hash(node))
 
         if node.is_solved():
             return node
 
-        self.recursion = max(self.max_recursion_reached, rec_lvl)
-        if rec_lvl > self.MAX_RECURSION:
-            self.break_flag = True
-            return None
-
-        for neighbour in self.get_neighbourhood(node):
-            self.processed += 1
-            neighbour_board = self.__dfs(neighbour, rec_lvl + 1)
-            if neighbour_board is not None:
-                return neighbour_board
+        if hash(node) not in self.expanded:
+            self.expanded[hash(node)] = node.steps
+            neighbors = self.get_neighbourhood(node)
+            for neighbour in neighbors:
+                if hash(neighbour) in self.expanded.keys():
+                    continue
+                self.visited += 1
+                self.processed += 1
+                child_board = self.__dfs(neighbour, recursion_lvl + 1)
+                if child_board is not None:
+                    return child_board
